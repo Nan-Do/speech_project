589b24f2489f0cc1ab03cb386245ba322702ebf0
Fernando Tarin Morales <icemanf@gmail.com>
2017-06-19 20:19:39 +0900

    Updated the script to be able to generate the virtual environment automatically

--- generate_virtual_env.bash
+++ generate_virtual_env.bash
@@ -1,6 +1,16 @@
+#! /bin/bash
+
+# Generate the virtualenv
 virtualenv speech_project
-cd speech_project
-source bin/activate
+
+# Copy the required files
+cp speech_recognizer.py speech_project
+cp -r audio speech_project
+
+# Activate the virtual environment
+. speech_project/bin/activate
+
+# Install the different required packages
 pip install numpy scipy
 pip install pillow
 pip install h5py
